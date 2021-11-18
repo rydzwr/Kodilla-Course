c3fe7164f2e06d2407e9877cb32170a4a76768a7
rydzwr <rydzwr@gmail.com>
2021-11-18 20:49:34 +0100

    Additional Task Forum Statistic

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCalculator.java
@@ -0,0 +1,51 @@
+package com.kodilla.testing.forum.statistics;
+
+
+public class StatisticsCalculator
+{
+    Statistics statistics;
+    public StatisticsCalculator(Statistics statistics)
+    {
+        this.statistics = statistics;
+    }
+
+    double postsAveragePerUser;
+    double commentsAveragePerUser;
+    double commentsAveragePerPost;
+
+    void calculateAdvStatistics()
+    {
+        double usersQuantity = statistics.usersNames().size();
+        double postQuantity = statistics.postsCount();
+        double commentsQuantity = statistics.commentsCount();
+
+        if (usersQuantity == 0)
+            throw new IllegalArgumentException("Zero Users!");
+
+        else if (postQuantity == 0)
+            throw new IllegalArgumentException("Zero Posts!");
+
+        else if (commentsQuantity == 0)
+            throw new IllegalArgumentException("Zero Comments!");
+
+
+        postsAveragePerUser = postQuantity / usersQuantity;
+        commentsAveragePerUser = commentsQuantity / usersQuantity;
+        commentsAveragePerPost = commentsQuantity / postQuantity;
+    }
+
+    void showStatistics()
+    {
+        double usersQuantity = statistics.usersNames().size();
+        double postQuantity = statistics.postsCount();
+        double commentsQuantity = statistics.commentsCount();
+
+        System.out.println("Users Quantity: " + usersQuantity);
+        System.out.println("Posts Quantity: " + postQuantity);
+        System.out.println("Comments Quantity: " + commentsQuantity);
+
+        System.out.println("Posts Average Per User: " + postsAveragePerUser);
+        System.out.println("Comments Average Per User: " + commentsAveragePerUser);
+        System.out.println("Comments Average Per Post: " + commentsAveragePerPost);
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticTestSuite.java
@@ -0,0 +1,131 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ForumStatisticTestSuite
+{
+    @BeforeAll
+    public static void beforeAllTests()
+    {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterAll
+    public static void afterAllTests()
+    {
+        System.out.println("All tests are finished.");
+    }
+
+    @BeforeEach
+    public void beforeEveryTest()
+    {
+
+        System.out.println("Preparing to execute test #");
+    }
+
+    @AfterEach
+    public void afterEveryTest()
+    {
+        System.out.println("Test Finished");
+    }
+
+    @Mock
+    private Statistics statisticsMock;
+
+    Random random = new Random();
+
+    int createThousandPosts()
+    {
+        int temporaryPosts = 1000;
+        return temporaryPosts;
+    }
+
+    ArrayList<String> generateUserNames(int usersCount)
+    {
+        ArrayList<String> users = new ArrayList<>();
+        for (int i = 0; i < usersCount; i++)
+            users.add("");
+
+        return users;
+    }
+
+    @Test
+    void normalTest()
+    {
+        //Given
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator(statisticsMock);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.usersNames()).thenReturn(generateUserNames(3));
+        when(statisticsMock.commentsCount()).thenReturn(28);
+
+        //When
+        statisticsCalculator.calculateAdvStatistics();
+
+        //Then
+        Assertions.assertEquals(statisticsCalculator.commentsAveragePerPost, 2.8);         // 28 / 10 = 2.8
+        Assertions.assertEquals(statisticsCalculator.commentsAveragePerUser, 28.0 / 3.0);  // 28 / 3 = 9.(333)
+        Assertions.assertEquals(statisticsCalculator.postsAveragePerUser, 10.0 / 3.0);     // 10 / 3 = 3.(333)
+    }
+
+    @Test
+    void ifNoPosts()
+    {
+        //Given
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator(statisticsMock);
+        when(statisticsMock.postsCount()).thenReturn(0);
+        when(statisticsMock.usersNames()).thenReturn(generateUserNames(3));
+        when(statisticsMock.commentsCount()).thenReturn(28);
+
+        //When
+
+
+        //Then
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () ->  statisticsCalculator.calculateAdvStatistics());
+        System.out.println("Test Successful, exception message: " + exception.getMessage());
+    }
+
+    @Test
+    void ifNoComments()
+    {
+        //Given
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator(statisticsMock);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.usersNames()).thenReturn(generateUserNames(3));
+        when(statisticsMock.commentsCount()).thenReturn(0);
+
+        //When
+
+
+        //Then
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () ->  statisticsCalculator.calculateAdvStatistics());
+        System.out.println("Test Successful, exception message: " + exception.getMessage());
+    }
+
+    @Test
+    void ifNoUsers()
+    {
+        //Given
+        StatisticsCalculator statisticsCalculator = new StatisticsCalculator(statisticsMock);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.usersNames()).thenReturn(generateUserNames(0));
+        when(statisticsMock.commentsCount()).thenReturn(28);
+
+        //When
+
+
+        //Then
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () ->  statisticsCalculator.calculateAdvStatistics());
+        System.out.println("Test Successful, exception message: " + exception.getMessage());
+    }
+
+
+}
