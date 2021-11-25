a6b6dfb2e6e704201379f3d8f1ecb74bc7a2063e
rydzwr <rydzwr@gmail.com>
2021-11-25 16:59:59 +0100

    TRY CATCH FINALLY

---
+++ kodilla-exception/src/test/java/com/kodilla/kodillaexception/KodillaExceptionApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kodilla.kodillaexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaExceptionApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
