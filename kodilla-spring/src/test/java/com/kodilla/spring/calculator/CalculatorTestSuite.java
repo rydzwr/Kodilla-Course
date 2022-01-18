cba2052157a21459115482b3db3f827fa3e134ba
rydzwr <rydzwr@gmail.com>
2022-01-18 20:23:11 +0100

    Zadanie: kalkulator z wyświetlaczem

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CalculatorTestSuite
+{
+    @Test
+    public void testCalculations()
+    {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
+        Calculator calculator = context.getBean(Calculator.class);
+        double a = 6.0;
+        double b = 2.0;
+
+        //When
+        double addResult = calculator.add(a, b);
+        double subResult = calculator.sub(a, b);
+        double mulResult = calculator.mul(a, b);
+        double divResult = calculator.div(a, b);
+
+        //Then
+        assertEquals(addResult, 8.0, 0.0001);
+        assertEquals(subResult, 4.0, 0.0001);
+        assertEquals(mulResult, 12.0, 0.0001);
+        assertEquals(divResult, 3.0, 0.0001);
+    }
+}
