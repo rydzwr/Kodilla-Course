18110000e139dc9c25796c6123b71b7f589ae50b
rydzwr <rydzwr@gmail.com>
2022-01-19 16:11:39 +0100

    Fixed Buckles

--- kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodillaSpringWebApplication
-{
-    public static void main(String[] args)
-    {
+public class KodillaSpringWebApplication {
+    public static void main(String[] args) {
         SpringApplication.run(KodillaSpringWebApplication.class, args);
     }
 }
