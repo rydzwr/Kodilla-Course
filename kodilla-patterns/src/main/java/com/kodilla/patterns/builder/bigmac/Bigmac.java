1c7a26b997bc4eea8e960f35723efcea1c8b79ea
rydzwr <rydzwr@gmail.com>
2022-01-22 19:42:01 +0100

    Zadanie: Komponujemy własnego BigMaca.

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -0,0 +1,87 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import java.util.ArrayList;
+
+public class Bigmac
+{
+    private String bun;
+    private int burgersQuantity;
+    private String sauceType;
+    private ArrayList<String> ingredients = new ArrayList();
+
+    public static class BigmacBulider
+    {
+        private String bun;
+        private int burgersQuantity;
+        private String sauceType;
+        private ArrayList ingredients = new ArrayList();
+
+        public BigmacBulider bun(String bun)
+        {
+            this.bun = bun;
+            return this;
+        }
+
+        public BigmacBulider burgersQuantity(int burgersQuantity)
+        {
+            this.burgersQuantity = burgersQuantity;
+            return this;
+        }
+
+        public BigmacBulider sauceType(String sauceType)
+        {
+            this.sauceType = sauceType;
+            return this;
+        }
+
+        public BigmacBulider ingredient(String ingredient)
+        {
+            ingredients.add(ingredient);
+            return this;
+        }
+
+        public Bigmac build()
+        {
+            return new Bigmac(bun, burgersQuantity, sauceType, ingredients);
+        }
+    }
+
+    public Bigmac(String bun, int burgersQuantity, String sauceType, ArrayList<String> ingredients)
+    {
+        this.bun = bun;
+        this.burgersQuantity = burgersQuantity;
+        this.sauceType = sauceType;
+        this.ingredients = ingredients;
+    }
+
+    public String getBun()
+    {
+        return bun;
+    }
+
+    public int getBurgersQuantity()
+    {
+        return burgersQuantity;
+    }
+
+    public String getSauceType()
+    {
+        return sauceType;
+    }
+
+    public ArrayList<String> getIngredients()
+    {
+        return ingredients;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Bigmac{" +
+                "bun='" + bun + '\'' +
+                ", burgersQuantity=" + burgersQuantity +
+                ", sauceType='" + sauceType + '\'' +
+                ", ingredients=" + ingredients +
+                '}';
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BigmacTestSuite
+{
+    @Test
+    public void testNewBigmac()
+    {
+        // Given
+
+        Bigmac bigmac = new Bigmac.BigmacBulider()
+                .ingredient("Onion")
+                .ingredient("Salad")
+                .ingredient("Tomato")
+                .ingredient("Cheese")
+                .bun("Classic")
+                .burgersQuantity(2)
+                .sauceType("Garlic Mayo")
+                .build();
+
+        System.out.println(bigmac);
+
+        // When
+
+        int howManyIngredients = bigmac.getIngredients().size();
+
+        // Then
+
+        Assertions.assertEquals(4, howManyIngredients);
+    }
+}
